9e7095e6221dd60176fd2e11947f8ca84f8b4da1
okcreating <oksibearus@gmail.com>
2024-09-10 21:16:34 +0100

    network layer is ready

--- NetworkLayer.playground/Contents.swift
+++ NetworkLayer.playground/Contents.swift
@@ -22,8 +22,6 @@ enum Path: String {
     case wrongURL = "/v0/neverfindable"
 }
 
-
-
 // MARK: Network Manager
 
 final class AsyncNetworkManager {
@@ -55,17 +53,68 @@ final class AsyncNetworkManager {
     private func fetchData() async throws -> Data {
 guard let url = createURL(path: path, queryItems: queryItems),
       let urlRequest = createRequest(url: url) else { throw NetworkError.invalidURL }
+
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else { throw NetworkError.invalidResponse }
+
+        return data
+    }
+
+    func fetch<T: Decodable>(_ model: T.Type) async throws -> T {
+        let data = try await fetchData()
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch {
+            throw NetworkError.decodingFailed
+        }
     }
 }
 
 extension AsyncNetworkManager {
     enum NetworkError: String, Error, LocalizedError {
         case invalidURL
-        case invalidRequest
+        case invalidResponse
         case decodingFailed
 
         var errorDescription: String? {
             return NSLocalizedString(rawValue, comment: "")
         }
+    }
+
+    func handleError(_ error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
+// MARK: Calling...
+
+let host = "api.magicthegathering.io"
+let path = Path.v1Cards
+//let path = Path.wrongURL
+let queryItems = [URLQueryItem(name: "name", value: "Opt|Black Lotus")]
+let model = Cards.self
+
+let networkManager = AsyncNetworkManager(host: host, path: path, queryItems: queryItems)
+
+Task {
+    do{
+        let result = try await networkManager.fetch(model)
+
+        result.cards.forEach( { card in
+            if card.name == "Opt" || card.name == "Black Lotus" {
+                print("""
+                    \(card.name.uppercased()) card:
+                    cmc: \(card.cmc ?? 0)
+                    set name: \(card.setName)
+                    number: \(card.number ?? "No number")
+                    power: \(card.power ?? "Doesn't matter")
+                    artist: \(card.artist ?? "Unknowm")\n
+                    """)
+                }
+            })
+    } catch {
+        networkManager.handleError(error)
     }
 }
